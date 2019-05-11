ba639a45c8dc06474c98643d8546e38194ae6cb8
Ivan <ivanyakovenko@MacBook-Pro-Ivan.local>
2019-05-12 00:12:05 +0300

    console

--- lab7/console.s
+++ lab7/console.s
@@ -1,1 +1,20 @@
-.section __TEXT,__text
+.data
+.extern max
+.extern act
+.extern string
+.text
+.globl conin
+.globl conout
+conin:
+sub     $8, %esp
+call    gets
+add     $8, %esp
+ret
+
+conout:
+sub     $8, %esp
+lea     string, %eax
+mov     %eax, (%esp)
+call    printf
+add     $8, %esp
+ret
